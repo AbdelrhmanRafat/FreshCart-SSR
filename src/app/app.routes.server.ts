492e5e8a98f4f7a3a101744c3cb6856f80d48e6b
Abdelrhman Rafat <eng.abdelrhman95@gmail.com>
2025-05-11 21:15:40 +0300

    Edit Server routes

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -2,11 +2,5 @@
 import { RenderMode, ServerRoute } from '@angular/ssr';
 
 export const serverRoutes: ServerRoute[] = [
-    { path: '', renderMode: RenderMode.Server },
-    // 🔴 خلي صفحات cart و wishlist تـتعرض من الـ Client فقط
-    { path: 'cart', renderMode: RenderMode.Client },
-    { path: 'wishlist', renderMode: RenderMode.Client },
-  
-    // 🟡 باقي الصفحات SSR
     { path: '**', renderMode: RenderMode.Server }
 ];
